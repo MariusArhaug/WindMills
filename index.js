26cad185912bc4d6fb111208b3ca9e20eb70a968
MariusArhaug <mariusarhaug@hotmail.com>
2021-08-29 16:09:57 +0200

    FEAT: finish all

--- index.js
+++ index.js
@@ -42,11 +42,14 @@ let isSVG = true;
 function swapArt() {
   if (isSVG) {
     display("#SVG-WindMill", "#canvas", "none", "block");
+    $("#currentArtHeader").html("Canvas-WindMill Mk 1");
     const bladeStyles = getComputedStyle($("#blades")[0]);
     const currentDuration = convertTimeToNumber(bladeStyles.animationDuration);
-    drawWindMill(currentDuration);
+    currentSpeed = 0.25 * currentDuration;
+    drawWindMill(1);
   } else {
     display("#SVG-WindMill", "#canvas", "block", "none");
+    $("#currentArtHeader").html("SVG-WindMill Mk 1");
   }
   isSVG = !isSVG;
 }
@@ -54,7 +57,6 @@ function swapArt() {
 let currentSpeed = 1;
 function drawWindMill(speed) {
   const canvas = $("#canvas")[0];
-  console.log(canvas);
   const ctx = canvas.getContext("2d");
   currentSpeed += 0.25 * speed;
   animateWindMill(ctx, canvas, 1, currentSpeed);
@@ -62,29 +64,32 @@ function drawWindMill(speed) {
 
 function animateWindMill(ctx, canvas, angle, speed) {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-
   ctx.save();
   ctx.translate(canvas.width / 2, canvas.height / 2);
   ctx.lineWidth = 3.25;
 
-  drawCircle(ctx, 0, 0, 5, "black");
-  const x1 = Math.cos((angle * Math.PI) / 180);
-  const y1 = Math.sin((angle * Math.PI) / 180);
+  const toRad = (num) => (num * Math.PI) / 180;
 
-  const x2 = Math.cos(((angle + 50) * Math.PI) / 180);
-  const y2 = Math.sin(((angle + 50) * Math.PI) / 180);
+  const x1 = Math.cos(toRad(angle));
+  const y1 = Math.sin(toRad(angle));
 
-  const x3 = Math.cos(((angle + 120) * Math.PI) / 180);
-  const y3 = Math.sin(((angle + 120) * Math.PI) / 180);
+  const x2 = Math.cos(toRad(angle + 50));
+  const y2 = Math.sin(toRad(angle + 50));
+
+  const x3 = Math.cos(toRad(angle + 120));
+  const y3 = Math.sin(toRad(angle + 120));
 
   ctx.lineWidth = 3.25;
-  // Draw base
-  drawLine(ctx, 0, 0, 0, 100, "C4C4C4");
-
   // Draw blades
-  drawLine(ctx, 10 * x1, 10 * y1, 40 * x1, 40 * y1, "black");
-  drawLine(ctx, -10 * x2, -10 * y2, -40 * x2, -40 * y2, "black");
-  drawLine(ctx, 10 * x3, 10 * y3, 40 * x3, 40 * y3, "black");
+  drawLine(ctx, 10 * x1, 10 * y1, 50 * x1, 50 * y1, "black");
+  drawLine(ctx, -10 * x2, -10 * y2, -50 * x2, -50 * y2, "black");
+  drawLine(ctx, 10 * x3, 10 * y3, 50 * x3, 50 * y3, "black");
+  //draw center
+  drawCircle(ctx, 0, 0, 5, "black");
+  // Draw base
+  drawLine(ctx, 0, 10, 0, 100, "#C4C4C4");
+
+  drawTriangle(ctx, 0, 60, "#C4C4C4");
 
   ctx.restore();
 
@@ -92,7 +97,6 @@ function animateWindMill(ctx, canvas, angle, speed) {
     animateWindMill(ctx, canvas, (angle + 0.5 * speed) % 360, speed);
   });
 }
-
 function drawCircle(ctx, x1, y1, radius, color) {
   ctx.beginPath();
   ctx.translate(0.5, 0.5);
@@ -109,5 +113,15 @@ function drawLine(ctx, x1, y1, x2, y2, color) {
   ctx.lineTo(x2, y2);
   ctx.strokeStyle = color;
   ctx.stroke();
+  ctx.closePath();
+}
+
+function drawTriangle(ctx, x, y, color) {
+  ctx.beginPath();
+  ctx.fillStyle = color;
+  ctx.moveTo(x, y);
+  ctx.lineTo(17.5 + x, 15 + y);
+  ctx.lineTo(-17.5 + x, 15 + y);
+  ctx.fill();
   ctx.closePath();
 }
